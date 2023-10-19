d92b4fac981febf9429797706635fd23ac36a2b8
Luis Abarca <luisabarca@gmail.com>
2023-10-18 23:32:44 -0600

    add tailwind and providers

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ApolloProvider } from "@apollo/client";
+import { NextUIProvider } from "@nextui-org/react";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { client } from "./apollo-client";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <NextUIProvider>
+          <App />
+        </NextUIProvider>
+      </Provider>
+    </ApolloProvider>
   </React.StrictMode>
 );
 
